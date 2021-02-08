59748ccdf8b44912e1ad77eb6c30017fc0d3c461
Aiman1017 <60610270+Aiman1017@users.noreply.github.com>
2021-02-08 15:28:00 +0800

    Add GameState to the code

--- tictactoedemo/MainActivity.kt
+++ tictactoedemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tictactoedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import com.example.tictactoedemo.databinding.ActivityMainBinding
@@ -12,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     //0 - cross, 1 - circle
     var activePlayer = 0
 
+    //Add GameState to keep track of the change
+    //The 2 represent game early State
+    //Will change base on activePlayer
+    val gameState = arrayListOf(2,2,2,2,2,2,2,2,2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     fun dropIn(view: View){
         val slotImage = view as ImageView
+        //Bind the tappedSlot with the slotImage
+        val tappedSlot = slotImage.tag.toString().toInt()
+        //bind the gameState with user tap or tapped Slot
+        gameState[tappedSlot] = activePlayer
+        Log.i("Game state", gameState.toString())
         slotImage.translationY = -2000f
         if(activePlayer == 0){
             slotImage.setImageResource(R.drawable.tic_tac_toe_x)
